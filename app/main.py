66a13574e922120d4d21fea5c2a812cecedf174f
faheel75 <161053362+faheel75@users.noreply.github.com>
2024-11-01 19:37:04 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
+from rag_pipeline import answer_question
+
+app = Flask(__name__)
+
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    data = request.json
+    question = data.get('question')
+    answer = answer_question(question)
+    return jsonify({'answer': answer})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
